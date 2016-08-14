ff338052878762a6853b4359aeb14a27db3f50a6
lixuechi <xli@algomau.ca>
2016-08-13 23:53:54 -0400

    + create basic cpp project with UserProfile and DEF

---
+++ cpp_project/agsq/DEF.cpp
@@ -0,0 +1,14 @@
+// Defines
+
+// country code
+public const int COUNTRY_NONE = -1;
+public const int COUNTRY_CHINA = 1;
+
+// language code
+public const int LANG_NONE = -1;
+public const int LANG_ENGLISH = 1;
+
+// gender 
+public const int GENDER_NONE = -1;
+public const int GENDER_MALE = 1;
+public const int GENDER_FEMALE = 2;
---
+++ cpp_project/agsq/UserProfile.cpp
@@ -0,0 +1,19 @@
+
+
+class UserProfile {
+
+public:
+	UserProfile(); 
+
+private:
+	int country;
+	int language;
+	int age;
+	int gender;
+	int[] preferredGenres;
+	int[] devices;
+	int[] alias;
+	int purchaseAction; // TODO
+	int[] playedGames;
+
+};
